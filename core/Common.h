bef5a34602843a18dcd33f089bfaabab490e616b
flokey <flokey@flokey-Inspiron-N4010.(none)>
2012-04-03 16:59:13 +0800

    modified:   Common.h 	modified:   SpatialPooler.h 	new file:   tags

--- core/Common.h
+++ core/Common.h
@@ -17,6 +17,7 @@ namespace htm07 {
     typedef int         coord_t;
     typedef unsigned    id_t;
     typedef int         data_t;
+    const int SPMAXDISTANCE = 0; // max distance to make sure two patterns are the same 
 
 #define DATA_SIZE       (sizeof(data_t))
 
@@ -25,7 +26,6 @@ namespace htm07 {
         size_t dims;
         size_t *max;
     };
-
+    data_t computeDistance(data_t *input1,data_t* input2)
 }   // namespace htm07
-
 #endif   /* ----- #ifndef _HTM07_COMMON_H__INC  ----- */
--- core/SpatialPooler.h
+++ core/SpatialPooler.h
@@ -10,18 +10,20 @@
 
 #ifndef  _HTM07_SPATIAL_POOLER_H__INC
 #define  _HTM07_SPATIAL_POOLER_H__INC
+#include "common.h"
+#include <vector>
 
 namespace htm07 {
 
 class SpPatternListT;
 
-class SpatialPooler
+class SpatialPoolerT
 {
 public:
-    SpatialPooler(NodeT *node)    {}
-    ~SpatialPooler()   {}
+    SpatialPoolerT(NodeT *node)    {}
+    ~SpatialPoolerT()   {}
 
-    void spExpose(data_t *input_data);
+    void spExpose(data_t *input_data);//main process
     bool learned() const;
 
     SpPatternListT* patternList();
