51ba607d371201534bf51fe02b5871e96c653d87
flokey <flokey@flokey-Inspiron-N4010.(none)>
2012-04-03 17:49:30 +0800

    modified:   Common.cpp 	modified:   Common.h

--- core/Common.cpp
+++ core/Common.cpp
@@ -12,5 +12,15 @@
 #include "Common.h"
 
 namespace htm07 {
-
+data_t computeDistance(data_t* input1,data_t* input2,size_t size1,size_t size2)
+{
+    if (size1!=size2)
+      return INFINITE;
+    data_t sum=0;
+    for (int i=0;i<size1;i++)
+    {
+        sum += (input1[i] - input2[i])*(input1[i]-input2[i]);
+    }
+    return sum;
+}
 }   // namespace htm07
--- core/Common.h
+++ core/Common.h
@@ -17,7 +17,8 @@ namespace htm07 {
     typedef int         coord_t;
     typedef unsigned    id_t;
     typedef int         data_t;
-    const int SPMAXDISTANCE = 0; // max distance to make sure two patterns are the same 
+    const data_t INFINITE = 200000000;
+    const data_t SPMAXDISTANCE = 0; // max distance to make sure two patterns are the same 
 
 #define DATA_SIZE       (sizeof(data_t))
 
@@ -25,7 +26,7 @@ namespace htm07 {
     {
         size_t dims;
         size_t *max;
-    };
-    data_t computeDistance(data_t *input1,data_t* input2)
+    }
+    data_t computeDistance(data_t* input1,data_t* input2,size_t size1,size_t size2)
 }   // namespace htm07
 #endif   /* ----- #ifndef _HTM07_COMMON_H__INC  ----- */
