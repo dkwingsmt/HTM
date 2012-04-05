ad4afc9eeb6db623af589d7410b01b5d8c011bee
flokey <flokey@flokey-Inspiron-N4010.(none)>
2012-04-05 14:13:04 +0800

    modified:   core/SpatialPooler.cpp 	modified:   core/SpatialPooler.h

--- core/SpatialPooler.cpp
+++ core/SpatialPooler.cpp
@@ -12,4 +12,41 @@
 
 namespace htm07 {
 
+void SpatialPooler::spLearn(data_t *input_data,size_t data_size)
+{
+    assert(data_size == _PatternList.getPatternSize());
+    bool neverAppeared = true;
+    for (int i=0;i<_PatternList.size();i++)
+    {
+       if (computeDistance(input_data,_PatternList.getPattern(i),data_size,_PatternList.getPatternSize())<=_MaxDist)
+       {
+           neverAppeared = false;
+      //     addTimeLine(preInputID,i);
+           break;
+       }
+    }
+    if (neverAppeared)
+    {
+        _PatternList.push(input_data);
+    //    addTimeLine(preInputID,_PatternList.getPatternSize()-1);   
+    }
+}
+
+data_t *spInference(data_t *input_data,size_t data_size)
+{
+    int minID = 0;
+    int minDist = INFINITE;
+    for (int i=0;i<_PatternList.size();i++)
+    {
+        int dist = computeDistance(input_data,_PatternList.getPattern(i),data_size,_PatternList.getPatternSize());
+        if (dist<minDist)
+        {
+            minID = i;
+            minDist = dist;
+        }
+    }
+    
+}
+
+
 }   // namespace htm07
--- core/SpatialPooler.h
+++ core/SpatialPooler.h
@@ -12,10 +12,26 @@
 #define  _HTM07_SPATIAL_POOLER_H__INC
 #include "common.h"
 #include <vector>
+#include <cassert>
 
 namespace htm07 {
 
-class SpPatternListT;
+
+typedef vector <int> OneDint_T;
+
+
+
+class SpPatternListT
+{
+public:
+    vector <data_t*> _Patterns;
+    size_t size();
+    size_t _Size;
+    size_t _PatternSize;
+    size_t getPatternSize();
+    data_t* getPattern(int i);
+    void push(data_t * input);
+};
 
 class SpatialPoolerT
 {
@@ -23,12 +39,16 @@ public:
     SpatialPoolerT(NodeT *node)    {}
     ~SpatialPoolerT()   {}
 
-    void spExpose(data_t *input_data);//main process
+    void spLearn(data_t *input_data,size_t data_size);//main process
+    data_t * spInference(data_t *input_data,size_t data_size);
     bool learned() const;
-
-    SpPatternListT* patternList();
+    int preInputID;
+    data_t _MaxDist;
+    SpPatternListT* _PatternList();
     size_t patternSize() const;
 
+    void addTimeLine(int prevID,int currID);
+    vector <OneDint_T> _TemporalTable;
 };
 
 }   // namespace htm07
