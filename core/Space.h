692a1901dd9652a1b51266bd29339af8ec3a4e1c
Mu Tong <dkwingsmt@gmail.com>
2012-04-10 21:42:02 +0800

    Corrected some type mismatch.

--- core/Space.h
+++ core/Space.h
@@ -24,16 +24,17 @@ public:
     // A subspace from an exsisted space within the given region
     SpaceT(const VecT* start_pos, const VecT* size,SpaceT * origin);
     ~SpaceT();
+
     inline coord_t getTotalCoord(id_t id, size_t dim) const;
-    inline coord_t getTotalLength(id_t id) const;
-    inline coord_t getSelfLength(id_t id) const;
+    inline coord_t getTotalLength(size_t dim) const;
+    inline coord_t getSelfLength(size_t dim) const;
     inline size_t getTotalSize() const;
     inline size_t getSelfSize() const;
 
     bool getSubSpace(const VecT* start_pos, const VecT* size, 
                      SpaceT **subspace);
 
-    bool isDerived() const{return !_Origin;}
+    bool isDerived() const      {return !_Origin;}
     size_t getDimension() const {return max.dims;}
 private:
     VecT max;
@@ -42,6 +43,7 @@ private:
     VecT _StartPos;
     SpaceT *_Origin;
     size_t *_SelfidProjector;
+
     friend bool copyFromSpaceToSubSpace(const data_t * source, data_t * dest, const SpaceT * originspace);
 };
 
@@ -50,30 +52,32 @@ coord_t SpaceT::getTotalCoord(id_t id, size_t dim) const
     return ((id/__idProjector[dim]) % getTotalLength(id));
 }
 
-coord_t SpaceT::getTotalLength(id_t id) const
+coord_t SpaceT::getTotalLength(size_t dim) const
 {
-    if(id <= max.dims)
-        return max.max[id];
+    if(dim <= max.dims)
+        return max.max[dim];
     else
         return -1;//to be modified
 }
 
-coord_t SpaceT::getSelfLength(id_t id) const
+coord_t SpaceT::getSelfLength(size_t dim) const
 {
-    if(id <= max.dims)
+    if(dim <= max.dims)
     {
         if(isDerived())  
-            return _SelfMax[id];
+            return _SelfMax[dim];
         else
-            return max.max[id];
+            return max.max[dim];
     }
     else
         return -1;//to be modified
 }
+
 size_t SpaceT::getTotalSize() const
 {
     return __idProjector[max.dims+1];
 }
+
 size_t SpaceT::getSelfSize() const
 {
     if(!isDerived())
