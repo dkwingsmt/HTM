818bfe52b5bfb155be0ca300e102302bccd7b3f3
Mu Tong <dkwingsmt@gmail.com>
2012-06-23 12:04:05 +0800

    testSpTableView now produce figure as Fig.10(b)

--- core/Introspection.h
+++ core/Introspection.h
@@ -18,13 +18,20 @@
 
 namespace htm07 {
 
+    typedef std::vector<OneDfloat_T> AdjMatT;
+
 class IntrospectionT
 {
 public:
     int getLayerNodeNum(const LayerT *layer) {  return layer->_NumNode;     }
+    NodeT const* const*getLayerNodes(const LayerT *layer)
+    {   return layer->_Nodes;    }
     int getLayerNodeCenterNum(const LayerT *layer, size_t node_id)
     {   return layer->_Nodes[node_id]->_Sp->getCentersNum();    }
-
+    const AdjMatT *getNodeSpAdjMat(const NodeT *p_node)
+    {   return &p_node->_Sp->_AdjMat;    }
+    const SpPatternListT *getNodeSpPatternList(const NodeT *p_node)
+    {   return p_node->_Sp->_PatternList;   }
 };
 
 }   // namespace htm07
